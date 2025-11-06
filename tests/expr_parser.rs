f85c347b771de13408e076e9be8a70e0b9cfa7a4
Nikolai Nedovodin <info@cputer.com>
2025-11-06 08:00:59 -0800

    docs(readme): add expression example (dev eval)

---
+++ tests/expr_parser.rs
@@ -0,0 +1,19 @@
+use mind::{eval, parser};
+
+#[test]
+fn precedence_and_parens() {
+    let module = parser::parse("1 + 2 * 3").unwrap();
+    assert_eq!(eval::eval_first_expr(&module).unwrap(), 7);
+
+    let module = parser::parse("(1 + 2) * 3").unwrap();
+    assert_eq!(eval::eval_first_expr(&module).unwrap(), 9);
+}
+
+#[test]
+fn division_and_zero_guard() {
+    let module = parser::parse("8 / 2").unwrap();
+    assert_eq!(eval::eval_first_expr(&module).unwrap(), 4);
+
+    let module = parser::parse("1 / 0").unwrap();
+    assert!(eval::eval_first_expr(&module).is_err());
+}
