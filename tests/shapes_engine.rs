a8a5a03b34c3765570342f9897e0b6f4553f4a2d
Nikolai Nedovodin <info@cputer.com>
2025-12-09 00:23:39 -0800

    Format shapes engine with stable rustfmt

--- tests/shapes_engine.rs
+++ tests/shapes_engine.rs
@@ -1,12 +1,22 @@
 use mind::shapes::engine::{
-    broadcast_shapes, infer_output_shape, is_elementwise, rule_for_op, ShapeErrorKind, ShapeRuleKind,
+    broadcast_shapes, infer_output_shape, is_elementwise, rule_for_op, ShapeErrorKind,
+    ShapeRuleKind,
 };
 
 #[test]
 fn rule_for_known_ops() {
-    assert_eq!(rule_for_op("tensor.relu"), Some(ShapeRuleKind::ElementwiseUnary));
-    assert_eq!(rule_for_op("tensor.add"), Some(ShapeRuleKind::ElementwiseBinary));
-    assert_eq!(rule_for_op("tensor.sum_all"), Some(ShapeRuleKind::ReduceAll));
+    assert_eq!(
+        rule_for_op("tensor.relu"),
+        Some(ShapeRuleKind::ElementwiseUnary)
+    );
+    assert_eq!(
+        rule_for_op("tensor.add"),
+        Some(ShapeRuleKind::ElementwiseBinary)
+    );
+    assert_eq!(
+        rule_for_op("tensor.sum_all"),
+        Some(ShapeRuleKind::ReduceAll)
+    );
     assert_eq!(rule_for_op("tensor.matmul"), Some(ShapeRuleKind::MatMul2D));
     assert!(rule_for_op("tensor.unknown").is_none());
 }
@@ -50,14 +60,15 @@ fn infer_elementwise_binary_broadcast() {
 
 #[test]
 fn infer_elementwise_unary_identity() {
-    let out = infer_output_shape("tensor.relu", &[&[4, 5, 6][..]]).expect("relu should preserve shape");
+    let out =
+        infer_output_shape("tensor.relu", &[&[4, 5, 6][..]]).expect("relu should preserve shape");
     assert_eq!(out, vec![4, 5, 6]);
 }
 
 #[test]
 fn infer_matmul_2d_ok() {
-    let out =
-        infer_output_shape("tensor.matmul", &[&[2, 3][..], &[3, 4][..]]).expect("matmul should work");
+    let out = infer_output_shape("tensor.matmul", &[&[2, 3][..], &[3, 4][..]])
+        .expect("matmul should work");
     assert_eq!(out, vec![2, 4]);
 }
 
@@ -72,7 +83,8 @@ fn infer_matmul_mismatched_inner_dim() {
 
 #[test]
 fn infer_reduce_all_to_scalar() {
-    let out = infer_output_shape("tensor.sum_all", &[&[2, 2][..]]).expect("sum_all should reduce to scalar");
+    let out = infer_output_shape("tensor.sum_all", &[&[2, 2][..]])
+        .expect("sum_all should reduce to scalar");
     // Rank-0 scalar represented as an empty shape.
     assert_eq!(out, Vec::<usize>::new());
 }
